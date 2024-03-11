ca258481d8a57f28204e2885f71c3234a6a01d30
Manish Kumar Singh <manish@xenonstack.com>
2024-03-11 17:16:17 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+
+import 'home.dart';
+
+void main() {
+  Stripe.publishableKey =
+  'pk_test_51L8PzGKEp9uhBKrrUaQGs8uNSewO9Lm85zYGJSJZ2I5nlYSnBtGRBa7Abky4uTxR4LYx2TMjkwJ48HJ6CZKVwLFo003wdImXQs';
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Stripe Gateway',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
